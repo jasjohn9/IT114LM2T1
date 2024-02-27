a9f1a44516505753605978804cd3ef7185fc47c9
jasjohn9 <154934528+jasjohn9@users.noreply.github.com>
2024-02-27 10:31:05 +0800

    Update Exercise4.aspx.cs

--- Module2Topic1/Exercise4.aspx.cs
+++ Module2Topic1/Exercise4.aspx.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +11,25 @@ namespace Module1Exercise1
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            // UnobtrusiveValidationMode.None to disable unobtrusive validation mode
+            ValidationSettings.UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
+        }
 
+        // Todo 4.5 When the user submits the form and all validations pass, display all the inputted texts in the "result" label
+        protected void Submit_Click(object sender, EventArgs e)
+        {
+            if (IsValid)
+            {
+                // Display the inputted texts in the "result" label
+                result.Text = $"Full Name: {fullName.Text}<br />" +
+                              $"Age: {age.Text}<br />" +
+                              $"Email: {email.Text}<br />" +
+                              $"Confirm Email: {confirmEmail.Text}";
+            }
+            else
+            {
+                result.Text = "Form is not valid";
+            }
         }
     }
 }
